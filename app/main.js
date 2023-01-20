cc1bba582044795141012fb6c8123f91709ab594
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2023-01-20 21:28:52 +0200

    Added textures

--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
-import './style.css'
+ import './style.css'
 
 import * as THREE from 'three'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { DirectionalLight, Vector3 } from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();  
 
@@ -16,11 +18,22 @@ renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30); 
 
-renderer.render( scene, camera )
+// renderer.render( scene, camera )
+
+const backgroundImage = new THREE.TextureLoader().load('Background.png');
+scene.background = backgroundImage;
+
+// const loader = new GLTFLoader();
+// loader.load()
+
 
 const geometry = new THREE.SphereGeometry(15, 32, 16);
-const material = new THREE.MeshStandardMaterial( {color: 0xffff00, wireframe: true} );
-const sphere = new THREE.Mesh( geometry, material );
+const texture = new THREE.TextureLoader().load("Albedo-diffuse.jpg");
+const normal = new THREE.TextureLoader().load("Normal.jpg");
+const sphere = new THREE.Mesh( geometry, new THREE.MeshStandardMaterial({ 
+  map: texture,
+  bumpMap: normal,
+}) );
 
 sphere.rotation.z -= 0.41;
 // sphere.rotation.z += 0.41;
@@ -32,7 +45,7 @@ sphere.rotation.z -= 0.41;
 scene.add(sphere);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-const directionalLight   = new THREE.DirectionalLight(0xffffff, 2);
+const directionalLight   = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(-45, 35, -30);
 directionalLight.target = sphere;
 
@@ -42,6 +55,10 @@ const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
   requestAnimationFrame( animate );
+
+
+
+
 
   controls.update();
 
