3eba091658c7bd42e71cdd99880f9587466be158
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2023-02-14 14:07:06 +0200

    started using context

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,15 @@
-import React, {Suspense, useEffect, useMemo, useRef, useState} from "react";
+import React, {createContext, Suspense, useContext, useEffect, useMemo, useRef, useState} from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import "./index.css";
 import { BoxGeometry } from "three";
+import { updateCamera } from "@react-three/fiber/dist/declarations/src/core/utils";
 
-export function WireframeGlobe(vertices?: string) {
+let globePath: string | undefined = undefined;
+const GlobeContext = createContext({path: globePath});
+
+export function WireframeGlobe() {
+  const path= useContext(GlobeContext)
   const sphere = useRef<THREE.Mesh>(null!)
   const [isRevolving, startRevolving] = useState(true)
   const [rotationSpeed, setRevolutionSpeed] = useState(0.1)
@@ -22,28 +27,29 @@ export function WireframeGlobe(vertices?: string) {
             startRevolving(false)
           else
             startRevolving(true)
+        case "f":
+          console.log(path)
       }
     }
 
     document.addEventListener("keypress", handle)
   })
 
-  if (typeof vertices == 'undefined'){
+  if (typeof path == 'string'){
     return(
       <mesh ref={sphere} rotation={[0, 0, -0.41]} position={[0, 0, 0]}>
         <sphereGeometry args={[15, 20]} />
-        <meshBasicMaterial wireframe={true} color={"red"} />
+        <meshBasicMaterial wireframe={true} color={path} />
       </mesh>
     )
   }
-  else{
-    return(
-      <mesh ref={sphere} rotation={[0, 0, -0.41]} position={[0, 0, 0]}>
-        <sphereGeometry args={[15, 20]} />
-        <meshBasicMaterial wireframe={true} color={vertices} />
-      </mesh>
-    )
-  }
+
+  return(
+    <mesh ref={sphere} rotation={[0, 0, -0.41]} position={[0, 0, 0]}>
+      <sphereGeometry args={[15, 20]} />
+      <meshBasicMaterial wireframe={true} color={"red"} />
+    </mesh>
+  )
   
   // Memoize the geometry
   // const geometry = <sphereGeometry args={[15, 20]} />;
@@ -55,13 +61,25 @@ export function WireframeGlobe(vertices?: string) {
 
 
 function ShowGlobe(){
-  return (
+  const {path, updatePath} = useContext(GlobeContext)
+  
+  useEffect(() => {
+    function handle(event: any){
+      if (event.key == "f")
+        updatePath("green")
+        console.log(path)
+    }
+  
+    document.addEventListener("keypress", handle)
+  })
+  
+   return (
     <>
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45}/>
 
       <PerspectiveCamera makeDefault fov={50} position={[0, 30, 50]}/>
 
-      <WireframeGlobe vertices={"yellow"}/>
+      <WireframeGlobe />
     </>
   )
 }
@@ -69,9 +87,11 @@ function ShowGlobe(){
 function App(){
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <Canvas>
-        <ShowGlobe />
-      </Canvas>
+      <GlobeContext.Provider value={globePath}>
+        <Canvas>
+          <ShowGlobe />
+        </Canvas>
+      </GlobeContext.Provider>
     </Suspense>
   )
 }
