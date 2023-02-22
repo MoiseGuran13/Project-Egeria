783bae0ae7a4fd0b0b22235514e3f0b7d9a4a430
MoiseGuran13 <60397889+MoiseGuran13@users.noreply.github.com>
2023-02-22 17:10:01 +0200

    finally added a background

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,8 @@ import { Suspense, useContext, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import "./index.css";
-// import { BoxGeometry } from "three";
-// import { updateCamera } from "@react-three/fiber/dist/declarations/src/core/utils";
 import GlobeContext from "./GlobeContext";
 import Globe, { WireframeGlobe } from "./Globe";
-import Background from "./Background.png"
 
 function ShowGlobe(){
   const {path, setPath} = useContext(GlobeContext)
@@ -21,22 +18,21 @@ function ShowGlobe(){
     document.addEventListener("keypress", handle)
   })
   
-  // console.log(require('./assets/Background.png'))
-   return (
+  return (
     <>
-      <div>
-        <img src={Background} alt={"Background"}/>
-      </div>
-      
       <OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45}/>
 
       <PerspectiveCamera makeDefault fov={50} position={[0, 30, 50]}/>
 
+      <Suspense fallback={<WireframeGlobe />}>
 
-      <WireframeGlobe />
+        <Globe />
+      
+      </Suspense>
     </>
   )
 }
+
 
 function App(){
   const [path, setPath] = useState(undefined)
@@ -44,9 +40,9 @@ function App(){
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <GlobeContext.Provider value={{path, setPath}}>
-        <Canvas>
-          <ShowGlobe />
-        </Canvas>
+          <Canvas>
+            <ShowGlobe />
+          </Canvas>
       </GlobeContext.Provider>
     </Suspense>
   )
